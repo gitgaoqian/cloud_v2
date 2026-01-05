219cf900ed6b2a70318da221e9d0856ad611a965
gitgaoqian <gaoqianneu@gmail.com>
2026-01-05 07:49:07 -0500

    delete print

--- test.py
+++ test.py
@@ -6,7 +6,5 @@ import time
 cmdstr = "rosparam get /img_pub_rate"
 cmdlist = shlex.split(cmdstr)
 a = subprocess.Popen(cmdlist,stdout=subprocess.PIPE)
-print a.stdout.read()
 a = rospy.get_param("/img_pub_rate")
-print a
 
